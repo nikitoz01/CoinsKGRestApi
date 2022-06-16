2aee7353edf257fc167787d8eaba05bff674afe9
nikit <nikitoz01@gmail.com>
2022-06-16 11:21:04 +0600

    Перешел на spring boot 2.7.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,26 +18,25 @@ repositories {
 
 dependencies {
 
-    implementation("org.springframework.boot:spring-boot-starter-security:2.6.7")
+    implementation("org.springframework.boot:spring-boot-starter-security:2.7.0")
 
-    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc:2.6.7")
-    implementation("org.springframework.boot:spring-boot-starter-webflux:2.6.7")
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc:2.7.0")
+    implementation("org.springframework.boot:spring-boot-starter-webflux:2.7.0")
     runtimeOnly("dev.miku:r2dbc-mysql:0.8.2.RELEASE")
     runtimeOnly("mysql:mysql-connector-java:8.0.29")
-
 
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.6")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.1-native-mt")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.2")
 
-    developmentOnly("org.springframework.boot:spring-boot-devtools:2.6.7")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.7") {
+    developmentOnly("org.springframework.boot:spring-boot-devtools:2.7.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.0") {
         exclude(module = "mockito-core")
     }
-    testImplementation("org.springframework.security:spring-security-test:5.6.3")
+    testImplementation("org.springframework.security:spring-security-test:5.7.1")
     testImplementation("com.ninja-squad:springmockk:3.1.1")
     testImplementation("io.mockk:mockk:1.12.4")
     testImplementation("io.projectreactor:reactor-test:3.4.18")
